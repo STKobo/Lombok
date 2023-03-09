1f9441e8a7a04b29da5001c4e3776ff07706f5a9
Stephane_tcha <stephanetcha@gmail.com>
2023-03-09 16:15:49 +0100

    Annotations Lombok

--- src/main/java/com/example/Lombok/pojo/Produit.java
+++ src/main/java/com/example/Lombok/pojo/Produit.java
@@ -1,63 +1,22 @@
 package com.example.Lombok.pojo;
 
-import java.util.Objects;
-
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+@FieldDefaults(level = AccessLevel.PRIVATE) /* attributs de classe encapsulés */
+@AllArgsConstructor /* Constructeurs avec paramètres */
+@NoArgsConstructor /* Constructeur sans paramètre */
+@Getter /* Getter pour fournir l'accès aux attibuts encapsulés */
+@Setter /* Setter pour instancier les attributs encapsulés */
+/*
+  Méthode utilisée pour comparer sémantiquement des objets de type Produit
+  Méthode utilisée pour renvoyer la valeur de hachage d'un objet Produit
+ */
+@EqualsAndHashCode(of = {"produitID", "marque", "prix"})
+@ToString(of = {"produitID", "marque", "prix"}) /* Méthode pour afficher l'objet sous forme de chaine de caractère */
 public class Produit {
     /* attributs de classe encapsulés */
-    private Long produitID;
-    private String marque;
-    private int prix;
-    /* Constructeurs avec paramètres */
-    public Produit(Long produitID, String marque, int prix) {
-        this.produitID = produitID;
-        this.marque = marque;
-        this.prix = prix;
-    }
-    /* Constructeur sans paramètre */
-    public Produit() {
-    }
-    /* Getter pour fournir l'accès aux attibuts encapsulés */
-    public Long getProduitID() {
-        return produitID;
-    }
-    public String getMarque() {
-        return marque;
-    }
-    public int getPrix() {
-        return prix;
-    }
-    /* Setter pour instancier les attributs encapsulés */
-    public void setProduitID(Long produitID) {
-        this.produitID = produitID;
-    }
-    public void setMarque(String marque) {
-        this.marque = marque;
-    }
-    public void setPrix(int prix) {
-        this.prix = prix;
-    }
-    /* Méthode utilisée pour comparer sémantiquement des objets de type Produit */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Produit produit)) return false;
-        return getPrix() == produit.getPrix()
-                && getProduitID().equals(produit.getProduitID())
-                && getMarque().equals(produit.getMarque());
-    }
-/* Méthode utilisée pour renvoyer la valeur de hachage d'un objet Produit */
-    @Override
-    public int hashCode() {
-        return Objects.hash(getProduitID(), getMarque(), getPrix());
-    }
-    /* Méthode pour afficher l'objet sous forme de chaine de caractère */
-    @Override
-    public String toString() {
-        return "Produit{" +
-                "produitID=" + produitID +
-                ", marque='" + marque + '\'' +
-                ", prix=" + prix + " euros" +
-                '}';
-    }
+    @NonNull
+    Long produitID;
+    String marque;
+    int prix;
 }
-
